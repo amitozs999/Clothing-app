58a3277719bd04591e5aec195958943935f9aebd
amitozs999 <ekas0001@gmail.com>
2023-09-01 17:06:24 +0530

    user context for sign in and sign out done

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,24 +1,47 @@
-import { Fragment } from 'react';
+import { Fragment , useContext} from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { UserContext } from '../../contexts/user.context';
+
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
+  
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser ? "signed in he user" : "sign out hogya");
+  console.log(currentUser);
+
+
   return (
     <Fragment>
+
+
       <div className='navigation'>
         <Link className='logo-container' to='/'>
           <CrwnLogo className='logo' />
         </Link>
+
+
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
-            SIGN IN
-          </Link>
+
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT 
+            </span>
+            
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              SIGN IN
+            </Link>
+          )}
+
+         
         </div>
       </div>
       <Outlet />
