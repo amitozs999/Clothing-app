7ab8e4dd339b55f70de94cfba847dc40737f76f6
amitozs999 <ekas0001@gmail.com>
2023-09-01 19:15:43 +0530

    product and cart context and checkout is  done

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import Authentication from './routes/authentication/authentication.component';
 
 
 import { Routes, Route } from 'react-router-dom';
+import Checkout from './routes/checkout/checkout.component';
 
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
-
-
+import Shop from './routes/shop/shop.component';
 const App = () => {
   
  
@@ -20,6 +16,7 @@ const App = () => {
       <Route index element={<Home />} />
       <Route path='shop' element={<Shop />} />
       <Route path='auth' element={<Authentication />} />
+      <Route path='checkout' element={<Checkout />} />
 
     </Route>
   </Routes>);
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,12 +5,16 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import { UserContext } from '../../contexts/user.context';
 
+import { CartContext } from '../../contexts/cart.context';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   console.log(currentUser ? "signed in he user" : "sign out hogya");
   console.log(currentUser);
 
@@ -41,8 +45,10 @@ const Navigation = () => {
             </Link>
           )}
 
-         
+        <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}   
+          {/* //cart open hoga toh return dropdown and show it */}
       </div>
       <Outlet />
     </Fragment>
