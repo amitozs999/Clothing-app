fa7702a1fe6570eb8227c0c7741a857d538021db
amitozs999 <ekas0001@gmail.com>
2023-09-03 18:57:52 +0530

    Using  user reducer inside context api instead of use state for checking and setting current user

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,7 +1,10 @@
 import { createContext, useState, useEffect } from 'react';
 
+
 import { onAuthStateChangedListener, createUserDocumentFromAuth, } from '../utils/firebase/firebase.utils';
 
+import {  useReducer } from 'react';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 
 export const UserContext = createContext({
@@ -9,9 +12,42 @@ export const UserContext = createContext({
   currentUser: null,
 });
 
+
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+
+
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+
+ // const [currentUser, setCurrentUser] = useState(null);
+//const value = { currentUser, setCurrentUser };
+
+const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+const setCurrentUser = (user) => dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+const value = { currentUser, };
+
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
@@ -23,6 +59,8 @@ export const UserProvider = ({ children }) => {
 
     return unsubscribe;
   }, []);
+
+ 
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
